e25d88235945008863901788d4ceb458487be370
Brian Howenstein <brian@clustertruck.com>
2017-09-21 10:21:56 -0400

    Initial commit

---
+++ new.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby -w
+
+require 'date'
+
+date      = Date.today # ISO 8601 Format
+filename  = "#{date.year}/#{date}.md"
+
+prev_date = Date.parse(Dir["**/*.{md,txt}"].map{|f| File.basename(f, ".*")}.sort_by {|f| Date.parse(f)}.last)
+prev_filename = "#{prev_date.year}/#{prev_date}.md"
+
+UNFINISHED_REGEX = /(?<=## TODO\n)(.|\n)+(?=\n## Accomplished)/
+unfinished = File.open(prev_filename, 'r').read.match(UNFINISHED_REGEX) || ""
+
+template = <<-TEMPLATE
+# #{date}
+
+## TODO
+#{unfinished.to_s.chomp}
+
+## Accomplished
+
+## Notes
+TEMPLATE
+
+File.open(filename, 'w') {|f| f.write(template) }
